9e8367e985087317abe7fe9d2175a7155f384ff5
asaunde78 <saundersasher78@gmail.com>
2023-04-11 03:43:45 -0400

    made it so that the extension now makes communications with the server to indicate what actions to do: reset or continue as normal

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 let links = {};
 
-
+chrome.tabs.onCreated.addListener
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       if (details.type === "image") {
@@ -20,23 +20,32 @@ chrome.webRequest.onBeforeRequest.addListener(
           links[details.tabId] += 1;
         }
         else {
+          // console.log(details);
+          chrome.cookies.get({url: details.initiator, name: "count"}, function(cookie) {
+            if (cookie) {
+              var cookieValue = cookie.value;
+              chrome.tabs.executeScript(details.tabId,{
+                code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "cookie${cookieValue}"}));`
+              });
+              fetch(`${proxy}add${details.tabId},${cookieValue}`)
+              console.log("Cookie value:", cookieValue);
+            } else {
+              console.log("Cookie not found");
+            }
+          });
+          
           links[details.tabId] = 1;
         }
+        // fetch(`${proxy}add${details.tabId},${5}`)
         console.log(`Redirecting: \turl: ${details.url}`);
-        console.log(details)
-        chrome.tabs.executeScript(details.tabId,{
-          code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "link_count${links[details.tabId]}"}));`
-        });
+        // console.log(details)
+        // chrome.tabs.executeScript(details.tabId,{
+        //   code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "link_count${links[details.tabId]}"}));`
+        // });
         console.log(links)
-        // chrome.cookies.getAll({ url: url }, function(cookies) {
-        //   console.log(cookies);
-        // });
-        // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        //   chrome.tabs.sendMessage(tabs[0].id, {valueToStore: JSON.stringify(links)});
-        //   console.log("sent message!")
-        // });
         
-        return {redirectUrl: `${proxy}${encodeURIComponent(details.url)}`}
+        
+        return {redirectUrl: `${proxy}${details.tabId}${encodeURIComponent(details.url)}`}
 
         // return {redirectUrl:`${strippedUrl}?resize=1:1`}
 
@@ -45,4 +54,28 @@ chrome.webRequest.onBeforeRequest.addListener(
     {urls: ["<all_urls>"]},
     ["blocking"]
   );
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    if (details.type == "image") {
+      resp = details.responseHeaders[2]["value"]
+      // console.log("Response body:", resp);
+      if(resp == "inline; filename=bad.png") {
+        console.log("bad");
+      }
+      if(resp == "inline; filename=good.png") {
+        console.log("good");
+      }
+      if(resp == "inline; filename=done.png") {
+        delete links[details.tabId];
+        chrome.tabs.executeScript(details.tabId,{
+          code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "DONE"}));`
+        });
+        console.log("done");
+      }
+    }
+  },
+  {urls: ["<all_urls>"]},
+  ['blocking', 'responseHeaders']
+);
   
