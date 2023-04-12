bba81e193fcbcabb5a0c1c3a139c2721d2c60254
asaunde78 <saundersasher78@gmail.com>
2023-04-11 23:35:31 -0400

    reworked trhe extension a little bit to communicate back and forth with the server/selenium in order to tell when to send redirects and what not. cookies get cleared once read now

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 let links = {};
-
+let grabbing = false;
 chrome.tabs.onCreated.addListener
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
@@ -16,33 +16,44 @@ chrome.webRequest.onBeforeRequest.addListener(
         if(!details.url.includes("googleusercontent.com") && !details.url.includes("favicon") && !details.url.includes("gstatic") &&  !details.url.includes("google.com")) {
           console.log(`${JSON.stringify(details)}`)
         }
-        if (details.tabId in links) {
+        
+        // fetch(`${proxy}add${details.tabId},${5}`)
+        console.log(`Redirecting: \turl: ${details.url}`);
+        // console.log(details)
+
+        if (details.tabId in links && grabbing) {
           links[details.tabId] += 1;
         }
         else {
           // console.log(details);
-          chrome.cookies.get({url: details.initiator, name: "count"}, function(cookie) {
-            if (cookie) {
-              var cookieValue = cookie.value;
+        chrome.cookies.get({url: details.initiator, name: "count"}, function(cookie) {
+          if (cookie) {
+            var cookieValue = cookie.value;
+            chrome.tabs.executeScript(details.tabId,{
+              code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "cookie${cookieValue}"}));`
+            });
+            fetch(`${proxy}add${details.tabId},${cookieValue}`)
+            console.log("Cookie value:", cookieValue);
+            chrome.cookies.remove({url:details.initiator,name:cookie.name}, function(removedCookie) {
+              console.log("Cookie deleted:", removedCookie);
               chrome.tabs.executeScript(details.tabId,{
-                code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "cookie${cookieValue}"}));`
+                code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "deleted a cookie"}));`
               });
-              fetch(`${proxy}add${details.tabId},${cookieValue}`)
-              console.log("Cookie value:", cookieValue);
-            } else {
-              console.log("Cookie not found");
-            }
-          });
+            });
+            grabbing = true
+      
+          } else {
+            console.log("Cookie not found");
+          }
+        });
           
           links[details.tabId] = 1;
+          
         }
-        // fetch(`${proxy}add${details.tabId},${5}`)
-        console.log(`Redirecting: \turl: ${details.url}`);
-        // console.log(details)
-        // chrome.tabs.executeScript(details.tabId,{
-        //   code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "link_count${links[details.tabId]}"}));`
-        // });
-        console.log(links)
+        chrome.tabs.executeScript(details.tabId,{
+          code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "link_count${links[details.tabId]}"}));`
+        });
+        // console.log(links)
         
         
         return {redirectUrl: `${proxy}${details.tabId}${encodeURIComponent(details.url)}`}
@@ -61,13 +72,18 @@ chrome.webRequest.onHeadersReceived.addListener(
       resp = details.responseHeaders[2]["value"]
       // console.log("Response body:", resp);
       if(resp == "inline; filename=bad.png") {
+        //potentially add a clear here so when the thread finishes getting everything we know to stop trying 
         console.log("bad");
       }
       if(resp == "inline; filename=good.png") {
+        
+        
         console.log("good");
       }
       if(resp == "inline; filename=done.png") {
-        delete links[details.tabId];
+        // links[details.tabId] = -1;
+        delete links[details.tabId]
+        grabbing = false
         chrome.tabs.executeScript(details.tabId,{
           code: `document.documentElement.appendChild(Object.assign(document.createElement("div"), {id: "DONE"}));`
         });
